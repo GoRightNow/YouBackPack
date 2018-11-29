862b854b5cdee82c0e2a710fff5b7739ba68807d
like <1134564996@qq.com>
2018-11-29 19:59:43 +0800

    更新路由

--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,43 @@ export default new Router({
     {
       path: '/PCView',
       name: 'PCView',
-      components: () => import('../PCView/login'),
-      children: [
-        {
-          path: '/admin',
-          name: 'admin',
-          components: () => import('../PCView/admin/storage')
-        }
-      ]
+      component: () => import('../PCView/login'),
+    },
+    {
+      path: '/PCView/admin/storage',
+      component: () => import('../PCView/admin/storage')
+    },
+    {
+      path: '/PCView/admin/sending',
+      component: () => import('../PCView/admin/sending')
+    },
+    {
+      path: '/PCView/admin/packageHistory',
+      component: () => import('../PCView/admin/packageHistory')
+    },
+    {
+      path: '/PCView/admin/userFeedback',
+      component: () => import('../PCView/admin/userFeedback')
+    },
+    {
+      path: '/PCView/admin/adminInfo',
+      component: () => import('../PCView/admin/adminInfo')
+    },
+    {
+      path: '/PCView/superAdmin/superAdmin',
+      component: () => import('../PCView/superAdmin/adminManage')
+    },
+    {
+      path: '/PCView/superAdmin/areaManage',
+      component: () => import('../PCView/superAdmin/areaManage')
+    },
+    {
+      path: '/PCView/superAdmin/packageManage',
+      component: () => import('../PCView/superAdmin/packageManage')
+    },
+    {
+      path: '/PCView/superAdmin/superAdminInfo',
+      component: () => import('../PCView/superAdmin/superAdminInfo')
     }
   ]
 })
